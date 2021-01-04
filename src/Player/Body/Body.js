f40db82a270c0b9e20d103f39ffef873e65059e4
Aniket Patel <aniket.r.21102001@gmail.com>
2021-01-04 22:52:43 +0530

    body css

--- src/Player/Body/Body.js
+++ src/Player/Body/Body.js
@@ -6,6 +6,14 @@ function Body({ spotify }) {
 	return (
 		<div className="body">
 			<Header spotify={spotify} />
+			<div className="body__info">
+				<img src="" alt="" />
+				<div className="body__infoText">
+					<strong>PLAYLIST</strong>
+					<h2>Discover weekly</h2>
+					<p>description</p>
+				</div>
+			</div>
 		</div>
 	);
 }
