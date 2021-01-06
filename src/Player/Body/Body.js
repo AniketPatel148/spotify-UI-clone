050f86a585fb53e3d168ac90c9501c0a2fd47613
Aniket Patel <aniket.r.21102001@gmail.com>
2021-01-06 21:39:05 +0530

    v1s1 complete

--- src/Player/Body/Body.js
+++ src/Player/Body/Body.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { useStateValue } from "../../Store/Store";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+
 import "./Body.css";
 import Header from "./Header/Header";
+import SongRow from "./SongRow/SongRow";
 
 function Body({ spotify }) {
 	const [{ discover_weekly }, dispatch] = useStateValue();
@@ -15,6 +20,21 @@ function Body({ spotify }) {
 					<h2>Discover weekly</h2>
 					<p>{discover_weekly?.description}</p>
 				</div>
+			</div>
+
+			<div className="body__songs">
+				<div className="body__icons">
+					<PlayCircleFilledIcon
+						className="body__shuffle"
+						onClick={playPlaylist}
+					/>
+					<FavoriteIcon fontSize="large" />
+					<MoreHorizIcon />
+				</div>
+
+				{discover_weekly?.tracks.items.map((item) => (
+					<SongRow playSong={playSong} track={item.track} />
+				))}
 			</div>
 		</div>
 	);
